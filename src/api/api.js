6e18d6503140661c90f2cdc24c77826bf1d7dfaf
Murodbek08 <arslonovmurodbek071@gmail.com>
2025-07-05 16:55:02 +0500

    ...

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const BASEURL = "https://newsapi.org/v2";
-const APIKEY = "edfb628062cf4234ba00dbdbce7898fb";
+const API_KEY = "edfb628062cf4234ba00dbdbce7898fb";
 
 const request = axios.create({
   baseURL: BASEURL,
-  headers: { Authorization: APIKEY },
+  headers: {
+    "x-api-key": API_KEY,
+  },
 });
 export default request;
