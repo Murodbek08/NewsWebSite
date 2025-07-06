ac55828a317c07b36f698414467522c1523885e4
Murodbek08 <arslonovmurodbek071@gmail.com>
2025-07-06 13:38:07 +0500

    zz

--- src/api/api.js
+++ src/api/api.js
@@ -6,7 +6,7 @@ const API_KEY = "edfb628062cf4234ba00dbdbce7898fb";
 const request = axios.create({
   baseURL: BASEURL,
   headers: {
-    "x-api-key": API_KEY,
+    Authorization: `Bearer ${API_KEY}`,
   },
 });
 export default request;
